90f505d48b322e796bbb343086041bb190c40c7f
mahendra <mahendradewangan195@gmail.com>
2024-09-27 12:35:44 +0530

    added iconbtn to return to home

--- src/app/archive/page.tsx
+++ src/app/archive/page.tsx
@@ -9,23 +9,31 @@ export default function Archive() {
   return (
     <> 
       <Flex width="s" direction='column' justifyContent='center' alignItems='center'>
-        {/* <IconButton
-          href="/"
-          icon="HiMiniArrowLeft"
-          size="l"
-          tooltip="Tooltip"
-          tooltipPosition="top"
-          variant="tertiary"
-        /> */}
-        <Heading
-            as='h2'
-            id="needs to  be changed"
-            variant='display-strong-s'
-            marginBottom='m'
-          >
-            {/* {about.work.title} */}
-            archive projects
-          </Heading>
+        <Flex fillWidth alignItems='center'   marginBottom='m' paddingX='s'>
+          <Flex flex={1}  alignItems='center'>
+            <IconButton
+              href='/'
+              icon="chevronLeft"
+              size="l"
+              tooltip="home"
+              tooltipPosition="bottom"
+              variant="tertiary"
+              />
+          </Flex>
+          <Flex flex={3}>
+            <Heading
+                as='h2'
+                id="needs to  be changed"
+                variant='display-strong-s'
+                
+               
+                >
+                {/* {about.work.title} */}
+              archive projects
+            </Heading>
+          </Flex>
+        </Flex>
+
         <ArchiveProject />
       </Flex>
     </>
